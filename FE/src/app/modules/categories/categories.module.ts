70f2bb68c82f95345e7f400bbfe28a08b539ccf8
codeware2022 <codewarelk@gmail.com>
2024-05-27 13:16:31 +0530

    add therapeutic category

--- FE/src/app/modules/categories/categories.module.ts
+++ FE/src/app/modules/categories/categories.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryMasterComponent } from './page/category-master/category-master.component';
 import { CategoryCardComponent } from './page/category-card/category-card.component';
+import { AddTherapeuticCategoryComponent } from './page/add-therapeutic-category/add-therapeutic-category.component';
+import { AddSubTherapeuticCategoryComponent } from './page/add-sub-therapeutic-category/add-sub-therapeutic-category.component';
 
 
 @NgModule({
   declarations: [
     CategoryMasterComponent,
-    CategoryCardComponent
+    CategoryCardComponent,
+    AddTherapeuticCategoryComponent,
+    AddSubTherapeuticCategoryComponent
   ],
   imports: [
     CommonModule,
